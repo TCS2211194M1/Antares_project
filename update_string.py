6519d7dca32051f6edf1163ad4344ab936ba2b00
JORGE AVILES <68437746+javilesm@users.noreply.github.com>
2024-02-25 11:19:06 -0600

    Update update_string.py

--- update_string.py
+++ update_string.py
@@ -1,29 +1,41 @@
-# update_string.py
 import os
 import fileinput
 import re
 import datetime
 
-# Variables
-script_directory = os.path.dirname(os.path.abspath(__file__)) # Directorio donde de encuentra este script
-log_dir = os.path.join(script_directory, "update_string_logs") # Directorio donde se almacenan los logs
-log_file = os.path.join(log_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_update_path_log.txt') # Archivo de log
+# Variables globales
+script_directory = os.path.dirname(os.path.abspath(__file__))  # Directorio donde se encuentra este script
+log_dir = os.path.join(script_directory, "update_string_logs")  # Directorio donde se almacenan los logs
+log_file = os.path.join(log_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_update_path_log.txt')  # Archivo de log
 
-cadena_antigua = "Project_Samava"
-cadena_nueva = "Antares_project"
+def solicitar_cadena(prompt):
+    while True:
+        cadena = input(prompt).strip()  # Eliminar espacios en blanco al principio y al final
+        confirmacion = input(f"Ha ingresado: '{cadena}'. ¿Es esto correcto? (S/N): ").strip().lower()
+        
+        if confirmacion == "s" or confirmacion == "s":
+            return cadena
+        elif confirmacion == "n":
+            print("Por favor, ingrese la cadena nuevamente.")
+        else:
+            print("Por favor, responda con 'S' o 'N'.")
 
 def cambiar_direcciones_en_archivo(ruta_archivo, cadena_antigua, cadena_nueva, log_file):
     try:
+        # Contador para el número de veces que se cambió la cadena en el archivo
+        contador_cambios = 0
+        
         # Reemplazar la cadena antigua por la nueva en cada línea del archivo
         with fileinput.FileInput(ruta_archivo, inplace=True, backup=".bak") as archivo:
             for linea in archivo:
                 # Reemplazar la cadena antigua por la nueva en la línea actual (insensible a mayúsculas/minúsculas)
-                nueva_linea = re.sub(re.escape(cadena_antigua), cadena_nueva, linea, flags=re.IGNORECASE)
+                nueva_linea, num_cambios = re.subn(re.escape(cadena_antigua), cadena_nueva, linea, flags=re.IGNORECASE)
+                contador_cambios += num_cambios  # Actualizar el contador con el número de cambios en esta línea
                 # Imprimir la nueva línea en el archivo
                 print(nueva_linea, end='')
 
-        # Informar que el cambio de direcciones ha sido completado en el archivo
-        mensaje = f"Cambio de '{cadena_antigua}' a '{cadena_nueva}' completado en el archivo: {ruta_archivo}"
+        # Informar que el cambio de direcciones ha sido completado en el archivo y cuántas veces se cambió
+        mensaje = f"Cambio de '{cadena_antigua}' a '{cadena_nueva}' completado en el archivo: {ruta_archivo}. Número de cambios: {contador_cambios}"
         print(mensaje)
         registrar_evento(log_file, mensaje)
     except Exception as e:
@@ -48,7 +60,15 @@ def registrar_evento(log_file, mensaje):
         log.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {mensaje}\n")
 
 def main():
+    # Crear directorio de logs si no existe
     crear_directorio_si_no_existe(log_dir)
+
+    
+    # Solicitar al usuario el valor de cadena_antigua
+    cadena_antigua = solicitar_cadena("Ingrese la cadena antigua: ")
+
+    # Solicitar al usuario el valor de cadena_nueva
+    cadena_nueva = solicitar_cadena("Ingrese la nueva cadena: ")
 
     # Buscar archivos PHP en el directorio del script
     archivos_php = buscar_archivos_php(script_directory)
